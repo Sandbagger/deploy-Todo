c0a9a842ece09a359c8b233171f2656a3b1b3531
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-11 17:28:27 +0000

    Added enviroment variables

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express'),
 	app = express(),
 	port = 3000;
 	bodyParser = require('body-parser'),
-	todoRoutes = require('./routes/todo');
+	todoRoutes = require('./routes/todo'),
+	path = require("path");
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.get('/', function (req, res){
 	res.send('Root route')
