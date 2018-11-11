9bd9ae444f107e798fc67f2dff9adc7c3095ce7b
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-11 19:41:12 +0000

    Removed Root route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
 	cors = require('cors'),
 	app = express(),
-	port = 3000;
+	port = process.env.PORT || 3000;
 	bodyParser = require('body-parser'),
 	todoRoutes = require('./routes/todo'),
 	path = require("path");
@@ -12,11 +12,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get('/', function (req, res){
-	res.send('Root route')
-})
+// app.get('/', function (req, res){
+// 	res.send('Root route')
+// })
 
 app.use('/api/todos', todoRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, function(){
 	console.log("Express server is running on port " + port)
