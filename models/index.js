bb1a8bc554e10fa28f4b9428b604d9b40f26da84
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-12 21:54:08 +0000

    Recommended settings

--- models/index.js
+++ models/index.js
@@ -1,7 +1,21 @@
 //When models dir is required it will look for this file first.
 const mongoose = require('mongoose');
 mongoose.set('debug', true); //catches errors
-mongoose.connect("mongodb://thisShouldBeSecret:qwert1@ds063779.mlab.com:63779/heroku_wmgdfcm5");
+
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
+var mongodbUri = 'mongodb://thisShouldBeSecret:qwert1@ds063779.mlab.com:63779/heroku_wmgdfcm5';
+ 
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
 
 mongoose.Promise = Promise;
 
