25f883c9879dda15642a861469649bcec07b5cb7
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-12 20:40:52 +0000

    Added new mLab variable for deployment

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 //When models dir is required it will look for this file first.
 const mongoose = require('mongoose');
 mongoose.set('debug', true); //catches errors
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo-api');
+mongoose.connect(process.env.MONGOLAB_PUCE_URI || 'mongodb://localhost/todo-api');
 
 mongoose.Promise = Promise;
 
