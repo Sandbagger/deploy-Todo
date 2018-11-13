497c18e06b7c80b49634d02e83a707c5f485f9bb
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-13 05:43:15 +0000

    Reverted to older version nd used default MONGODB_URI

--- models/index.js
+++ models/index.js
@@ -1,22 +1,7 @@
 //When models dir is required it will look for this file first.
 const mongoose = require('mongoose');
 mongoose.set('debug', true); //catches errors
-
-       
- 
-var mongodbUri = 'mongodb://thisShouldBeSecret:qwert1@ds063779.mlab.com:63779/heroku_wmgdfcm5';
-
-mongoose.connect(mongodbUri, {
-    useMongoClient: true
-});
-var conn = mongoose.connection;             
- 
-conn.on('error', console.error.bind(console, 'connection error:'));  
- 
-conn.once('open', function() {
-  // Wait for the database connection to establish, then start the app.                         
-});
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo-api');
 
 mongoose.Promise = Promise;
 
