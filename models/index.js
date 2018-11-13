4d76ff07927ebe6f288eef0d1f5d92a77158687e
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-13 06:02:51 +0000

    Trying to conect to database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 //When models dir is required it will look for this file first.
 const mongoose = require('mongoose');
 mongoose.set('debug', true); //catches errors
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo-api');
+var options = { keepAlive: 300000, connectTimeoutMS: 30000, useNewUrlParser: true};
+mongoose.connect("mongodb://todo:2a4b1c@ds261253.mlab.com:61253/heroku_081pxq13", options);
 
 mongoose.Promise = Promise;
 
