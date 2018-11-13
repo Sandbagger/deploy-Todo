13ea137360135af61c984f160ad03f504f7f551f
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-13 06:56:38 +0000

    Api keeps serving HTML. Changed app.get from wildcard to / to test whetehr I am matching every route to HTML

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use('/api/todos', todoRoutes);
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
