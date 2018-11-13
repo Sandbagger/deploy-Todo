010672a18325aa05ad63c7ab749344f10285779f
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-13 05:15:04 +0000

    Added .env file with dotenv to store envio varaibles

--- models/index.js
+++ models/index.js
@@ -1,22 +1,20 @@
 //When models dir is required it will look for this file first.
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Reference .env vars off of the process.env object
+MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+
 mongoose.set('debug', true); //catches errors
-
-       
- 
-var mongodbUri = 'mongodb://thisShouldBeSecret:qwert1@ds063779.mlab.com:63779/heroku_wmgdfcm5';
-
-mongoose.connect(mongodbUri, {
-    useMongoClient: true
-});
-var conn = mongoose.connection;             
- 
-conn.on('error', console.error.bind(console, 'connection error:'));  
+   
+const conn = mongoose.connection;             
  
 conn.once('open', function() {
   // Wait for the database connection to establish, then start the app.                         
 });
-
 
 mongoose.Promise = Promise;
 
