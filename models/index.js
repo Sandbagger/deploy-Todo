98b35e00a9f8ffde9427102d5f3ed2e8deea8f10
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-13 06:21:46 +0000

    Trying to conect to database

--- models/index.js
+++ models/index.js
@@ -2,7 +2,21 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true); //catches errors
 
-mongoose.connect(process.env.MONGODB_URI);
+       
+ 
+var mongodbUri = 'mongodb://todo:2a4b1c.mlab.com:63779/heroku_wmgdfcm5';
+
+mongoose.connect(mongodbUri, {
+    useMongoClient: true
+});
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
 
 mongoose.Promise = Promise;
 
