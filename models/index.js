33d3d2b1d7bd9d3345f96608ba37de91a94cebd1
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-13 06:34:03 +0000

    Trying to connect to db. Hardcoded db uri

--- models/index.js
+++ models/index.js
@@ -1,19 +1,14 @@
 //When models dir is required it will look for this file first.
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 mongoose.set('debug', true); //catches errors
-
-       
+ 
  
 var mongodbUri = 'mongodb://todo:2a4b1c.mlab.com:63779/heroku_wmgdfcm5';
-=======
-const dotenv = require('dotenv').config();
->>>>>>> 28c54d3490e334c4895843f3f4bdd820e50ce729
 
 // Reference .env vars off of the process.env object
-mongoose.connect(process.env.MONGODB_URI, function(err, db) {
+mongoose.connect(mongodbUri, function(err, db) {
   if(!err) {
-    console.log("We are connected");
+    console.log("We are connected to db");
   }
 })
 mongoose.set('debug', true); //catches errors
