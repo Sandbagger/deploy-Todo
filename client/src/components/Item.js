94339ab79e1bfaa234d3b67e7c269bee6fa547b1
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-15 07:26:40 +0000

    Refactored Item component using correct Material-UI apis

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Grid from '@material-ui/core/Grid';
 import {ListItemText, Typography } from '@material-ui/core';
 import Delete from './Delete';
@@ -10,32 +11,19 @@ const Item = (props) => (
         button
         onClick={() => props.onClick(props.completed, props._id)}
         style={{textDecoration: props.completed ? 'line-through' : 'none'}}>
-    <Grid container 
-    spacing={0}
-    alignItems='center'
-    justify='space-between'
-    wrap='noWrap'> 
-          <Grid 
-            item>
+    
             <ListItemText 
               inset>
-                <Typography wrap='noWrap'>
+                <Typography noWrap>
                     {props.name}
                     </Typography>
             </ListItemText>
-            </Grid>
-            
-            <Grid item>
-            <ListItem  
-              button
-              onClick={(e) => {   
+            <ListItemSecondaryAction onClick={(e) => {
                   e.stopPropagation();
                 props.onDelete(props._id)}}>
-                 <Delete completed={props.completed}/>
-            </ListItem>
-            </Grid>
-            
-        </Grid>
+               <Delete 
+                 isCompleted={props.completed}/>
+            </ListItemSecondaryAction>
   </ListItem>
 );
 
