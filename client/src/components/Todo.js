39f6b3dfa54f2a5c8dac902a87940d71d7c369f0
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-15 23:56:15 +0000

    Changed Request path to production

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -4,7 +4,7 @@ import Item from './Item.js';
 import Form from './Form.js';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-const url = 'http://localhost:3001/api/todo/';
+const url = '/api/todo/';
 
 const style = {
     minHeight: "500px",
