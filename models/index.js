80c5eb7900367806e0f70061371c86607fe384d3
Unknown <wneal_19@yahoo.co.uk>
2018-11-16 06:44:44 +0000

    Removed unused variable.

--- models/index.js
+++ models/index.js
@@ -1,17 +1,15 @@
-//When models dir is required it will look for this file first.
+//When models is required it will look for this file first.
 const mongoose = require('mongoose');
 mongoose.set('debug', true); 
  
 const dbDriver = process.env.MONGODB_URI || "mongodb://localhost/todo-api";
-console.log(dbDriver)
+
 mongoose.connect(dbDriver, function(err, db) {
   if(!err) {
     console.log("Connected to " + dbDriver);
   }
 })
-   
-const conn = mongoose.connection;             
-
+               
 mongoose.Promise = Promise;
 
 module.exports.Todo = require("./todo");
