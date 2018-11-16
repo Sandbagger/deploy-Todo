5f5c74c7eafeab4353a0b7d140adb4892554cd8a
Unknown <wneal_19@yahoo.co.uk>
2018-11-16 06:39:18 +0000

    Deleted unused import statement. Refactored code spacing.

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Grid from '@material-ui/core/Grid';
 import {ListItemText, Typography, Button } from '@material-ui/core';
 import Delete from './Delete';
 
@@ -12,16 +11,18 @@ const Item = (props) => (
         onClick={() => props.onClick(props.completed, props._id)}
         style={{textDecoration: props.completed ? 'line-through' : 'none'}}>
     
-            <ListItemText 
-              inset>
+            <ListItemText>
                 <Typography noWrap>
                     {props.name}
-                    </Typography>
+                </Typography>
             </ListItemText>
+
             <ListItemSecondaryAction onClick={(e) => {
                   e.stopPropagation();
                 props.onDelete(props._id)}}>
-               <Button ><Delete isCompleted={props.completed}/></Button>
+                  <Button >
+                    <Delete isCompleted={props.completed}/>
+                  </Button>
             </ListItemSecondaryAction>
   </ListItem>
 );
