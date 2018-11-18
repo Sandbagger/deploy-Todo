15cad5227bc2860a9e77a4f5e6c802ffa98e433f
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-18 21:25:31 +0000

    Removed unused import

--- client/src/components/Delete.js
+++ client/src/components/Delete.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import DeleteIcon from '@material-ui/icons/Delete';
+
 
 const Delete = props => (
     props.isCompleted ? <DeleteForeverIcon /> : null
