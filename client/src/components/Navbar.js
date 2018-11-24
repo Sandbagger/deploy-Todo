658dabfeffd6f8a2ab7ef27f41e641ec741efddd
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-24 13:53:09 +0000

    Added button to link to Github repo on Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const NavBar = () => {
     return(
@@ -9,9 +11,19 @@ const NavBar = () => {
         <AppBar 
             position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
-                Todo List
-                </Typography>
+                <Grid container justify="space-between" alignItems="center">
+                    <Grid item>
+                        <Typography variant="title" color="inherit">
+                             Todo List
+                        </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Button color="inherit" varient="contained" href="https://github.com/Sandbagger/deploy-todo"> 
+                                Github
+                            </Button>
+                        </Grid>
+                </Grid>
+              
             </Toolbar>
         </AppBar>
         </div>
